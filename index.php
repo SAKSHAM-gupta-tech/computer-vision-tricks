1d77f5d53f9a399b523bc3ad77f5784c0393338b
SAKSHAM-gupta-tech <66868904+SAKSHAM-gupta-tech@users.noreply.github.com>
2020-07-19 15:51:48 +0530

    commit1

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <html>
 <body>
-<body bgcolor='red'>
-<marquee><h1>WELCOME </hi></marquee>
-<img src="https://mysecond-bucket.s3.ap-south-1.amazon.com/pubg.jpg"
+<body bgcolor='cyan'>
+<marquee><h1>THANKS VIMAL SIR FOR PROVIDING THIS KNOWLEDGE</h1></marquee>
+
+<img src="https://images.yourstory.com/cs/5/803961002d6d11e9aa979329348d4c3e/Vimal21574008900398png?fm=png&auto=format&blur=500.jpg" alt="pubg" width="600" height="600">
+<img src="http://httpd.apache.org/.jpg" alt="saksham" width="100" height="100">
 </body>
 </html>
